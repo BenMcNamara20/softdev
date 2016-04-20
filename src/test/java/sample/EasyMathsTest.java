3c9bbef208a1d5a83acda5034cf88d712a8e3f99
k00199363 <k00199363@MY_8A106_17.student.litdom.lit.ie>
2016-04-20 17:02:49 +0100

    Initial commit of a project board

---
+++ src/test/java/sample/EasyMathsTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author k00199363
+ */
+public class EasyMathsTest {
+    
+    public EasyMathsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    
+    
+
+    
+}
